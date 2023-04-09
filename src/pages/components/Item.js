4ec29cefbaef8cd52fe792967cbc7582108d0b8f
Magistra <magistraagis@gmail.com>
2023-04-09 19:37:17 +0800

    fix item

--- src/pages/components/Item.js
+++ src/pages/components/Item.js
@@ -4,11 +4,11 @@ import gambarItem from '../images/frame 28.svg';
 import Link from 'next/link';
 export default function Item() {
   return (
-    <div className='border border-black m-4 w-3/12'>
-      <Link className=' ' href="/detail">
-        <Image src={gambarItem} alt='items' height={500} />
-        <div className='my-4 mx-2'>
-          <h1 className='font-bold text-slate-900'>Judul item</h1>
+    <div className='border m-4 w-3/12 rounded-lg'>
+      <Link href="/detail">
+        <Image src={gambarItem} alt='items' height={500} className='rounded-t-lg' />
+        <div className='m-4'>
+          <h1 className='font-bold text-slate-900 text-xl'>Judul item</h1>
           <p className='text-slate-500'>Harga item</p>
         </div>
       </Link>
